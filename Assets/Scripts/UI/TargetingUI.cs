953f1f714892fd736e560f84b4e3d87328218d6d
Mana <manakouwenberg@gmail.com>
2022-12-15 13:52:40 +0100

    Fucking kickass Targeting System implemented

--- Assets/Scripts/UI/TargetingUI.cs
+++ Assets/Scripts/UI/TargetingUI.cs
@@ -5,9 +5,15 @@ using UnityEngine.UI;
 
 public class TargetingUI : MonoBehaviour
 {
-    public LayerMask hittableLayers;
+    public LayerMask hittableLayers, UILayer;
     public GameObject targetInfoPanel, targetingReticle;
     public Text hpText, armorText, weaponText, secWeaponText, nameText;
+
+    public Text reticleNameText;
+    public GameObject[] chevrons;
+
+    public float baseDistanceReticle = 12.0f;
+    public float scaleSpeed;
 
     private void FixedUpdate()
     {
@@ -36,6 +42,28 @@ public class TargetingUI : MonoBehaviour
                 weaponText.text = enemy.weaponName;
                 secWeaponText.text = enemy.secondaryWeaponName;
                 nameText.text = enemy.enemyName;
+
+                reticleNameText.text = enemy.enemyName;
+
+                float hpRatio = enemy.hp / enemy.maxHP;
+                for (int i = 0; i < chevrons.Length; i++)
+                {
+                    chevrons[i].SetActive(hpRatio > (1.0f / (float)chevrons.Length) * i);                   
+                }
+
+                RaycastHit hit1;
+                if(Physics.Raycast(enemy.transform.position, (Camera.main.transform.position - enemy.transform.position).normalized, out hit1, 50, UILayer))
+                {
+                    targetingReticle.transform.position = hit1.point;
+                    float scaleRatio = Mathf.Clamp(2.0f - Mathf.Pow(hit1.distance / baseDistanceReticle, scaleSpeed), 0.25f, 2.0f);
+                    targetingReticle.transform.localScale = new Vector3(scaleRatio, scaleRatio, scaleRatio);
+                }
+                else
+                {
+                    targetingReticle.SetActive(false);
+                }
+
+
             }
 
             if(dob != null)
@@ -43,11 +71,11 @@ public class TargetingUI : MonoBehaviour
                 targetInfoPanel.SetActive(true);
                 targetingReticle.SetActive(false);
 
-                hpText.text = ((int)dob.HP).ToString();
-                armorText.text = ((int)dob.armor).ToString();
+                hpText.text = (dob.HP).ToString();
+                armorText.text = (dob.armor).ToString();
                 weaponText.text = "";
                 secWeaponText.text = "";
-                nameText.text = dob.objectName;
+                nameText.text = dob.objectName;                
             }
         }
     }
